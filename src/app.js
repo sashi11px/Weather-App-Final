bab74bcf023eab16870f14ea8b7d2080cdd40e8a
sashi11px <djsashi11@gmail.com>
2022-12-05 08:28:36 -0500

    injected HTML into JS

--- src/app.js
+++ src/app.js
@@ -52,6 +52,50 @@ function search(city) {
 let searchCity = document.querySelector("form");
 searchCity.addEventListener("submit", newCity);
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  forecastHTML =
+    forecastHTML +
+    `
+              <div class="col-2">
+                <div class="weather-forecast-date">Thu</div>
+                <img
+                  src="http://openweathermap.org/img/wn/50d@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max"> 18° </span>
+                  <span class="weather-forecast-temperature-min"> 12° </span>
+                </div>
+              </div>
+            
+            `;
+
+  forecastHTML =
+    forecastHTML +
+    `
+              <div class="col-2">
+                <div class="weather-forecast-date">Thu</div>
+                <img
+                  src="http://openweathermap.org/img/wn/50d@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max"> 18° </span>
+                  <span class="weather-forecast-temperature-min"> 12° </span>
+                </div>
+              </div>
+            
+            `;
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function currentWeather(response) {
   let maintemp = document.querySelector("#temperature");
   let maincity = document.querySelector("#maincity");
@@ -95,3 +139,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Paros, Greece");
+displayForecast();
