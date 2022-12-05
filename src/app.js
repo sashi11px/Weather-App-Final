1b851e22195aa9ed7e0710393d7e35c7ca10e468
sashi11px <djsashi11@gmail.com>
2022-12-05 09:23:25 -0500

    Update app.js
    added api key
    new function - get forecast

--- src/app.js
+++ src/app.js
@@ -52,7 +52,7 @@ function search(city) {
 let searchCity = document.querySelector("form");
 searchCity.addEventListener("submit", newCity);
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
 
@@ -81,6 +81,12 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "24f1c0075dd230a627ed2df630716f94";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function currentWeather(response) {
   let maintemp = document.querySelector("#temperature");
   let maincity = document.querySelector("#maincity");
@@ -101,7 +107,7 @@ function currentWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 
-  console.log(response.data);
+  getForecast(response.data.coord);
 }
 
 function displayFahrenheitTemperature(event) {
@@ -126,4 +132,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Paros, Greece");
-displayForecast();
